45bdfef8f173d9a24991d30f0d35660c59293a51
Christopher Tiso <ctiso@udel.edu>
2022-05-02 14:05:49 -0400

    Course done

--- Server/Controllers/Course/CourseController.cs
+++ Server/Controllers/Course/CourseController.cs
@@ -33,7 +33,7 @@ namespace SWARM.Server.Controllers.Crse
 
         [HttpGet]
         [Route("GetCourses")]
-        public async Task<IActionResult> GetCourses()
+        public async Task<IActionResult> Get()
         {
             List<Course> lstCourses = await _context.Courses.OrderBy(x => x.CourseNo).ToListAsync();
             return Ok(lstCourses);
@@ -41,7 +41,7 @@ namespace SWARM.Server.Controllers.Crse
 
         [HttpGet]
         [Route("GetCourses/{pCourseNo}")]
-        public async Task<IActionResult> GetCourse(int pCourseNo)
+        public async Task<IActionResult> Get(int pCourseNo)
         {
             Course itmCourse = await _context.Courses.Where(x => x.CourseNo == pCourseNo).FirstOrDefaultAsync();
             return Ok(itmCourse);
@@ -49,7 +49,7 @@ namespace SWARM.Server.Controllers.Crse
 
         [HttpDelete]
         [Route("DeleteCourse/{pCourseNo}")]
-        public async Task<IActionResult> DeleteCourse(int pCourseNo)
+        public async Task<IActionResult> Delete(int pCourseNo)
         {
             Course itmCourse = await _context.Courses.Where(x => x.CourseNo == pCourseNo).FirstOrDefaultAsync();
             _context.Remove(itmCourse);
@@ -57,24 +57,72 @@ namespace SWARM.Server.Controllers.Crse
             return Ok();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] CourseDTO _CourseDTO)
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Course _Course)
         {
+            bool bExist = false;
             var trans = _context.Database.BeginTransaction();
             try
             {
-                var existCourse = await _context.Courses.Where(x => x.CourseNo == _CourseDTO.CourseNo).FirstOrDefaultAsync();
+                var _Crse = await _context.Courses.Where(x => x.CourseNo == _Course.CourseNo).FirstOrDefaultAsync();
+                if (_Crse == null)
+                {
+                    bExist = false;
+                    _Crse = new Course();
+                }
+                else
+                    bExist = true;
 
-                existCourse.Cost = _CourseDTO.Cost;
-                existCourse.Description = _CourseDTO.Description;
-                existCourse.Prerequisite = _CourseDTO.Prerequisite;
-                existCourse.PrerequisiteSchoolId = _CourseDTO.PrerequisiteSchoolId;
-                existCourse.SchoolId = _CourseDTO.SchoolId;
-                _context.Update(existCourse);
+                _Crse.Cost = _Course.Cost;
+                _Crse.Description = _Course.Description;
+                _Crse.Prerequisite = _Course.Prerequisite;
+                _Crse.PrerequisiteSchoolId = _Course.PrerequisiteSchoolId;
+                _Crse.SchoolId = _Course.SchoolId;
+                if (bExist)
+                {
+                    _context.Courses.Update(_Crse);
+                }
+                else
+                    _context.Courses.Add(_Crse);
+
                 await _context.SaveChangesAsync();
                 trans.Commit();
 
-                return Ok(_CourseDTO.CourseNo);
+                return Ok(_Course.CourseNo);
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Course _Course)
+        {
+            bool bExist = false;
+            var trans = _context.Database.BeginTransaction();
+            try
+            {
+                var _Crse = await _context.Courses.Where(x => x.CourseNo == _Course.CourseNo).FirstOrDefaultAsync();
+                if (_Crse != null)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Record Exists");
+
+                }
+
+                _Crse.Cost = _Course.Cost;
+                _Crse.Description = _Course.Description;
+                _Crse.Prerequisite = _Course.Prerequisite;
+                _Crse.PrerequisiteSchoolId = _Course.PrerequisiteSchoolId;
+                _Crse.SchoolId = _Course.SchoolId;
+
+                _context.Courses.Add(_Crse);
+
+                await _context.SaveChangesAsync();
+                trans.Commit();
+
+                return Ok(_Course.CourseNo);
             }
             catch (Exception ex)
             {
@@ -84,63 +132,63 @@ namespace SWARM.Server.Controllers.Crse
         }
 
 
+        //    [HttpPost]
+        //    [Route("GetCourses")]
+        //    public async Task<DataEnvelope<CourseDTO>> GetCoursesPost([FromBody] DataSourceRequest gridRequest)
+        //    {
+        //        DataEnvelope<CourseDTO> dataToReturn = null;
+        //        IQueryable<CourseDTO> queriableStates = _context.Courses
+        //                .Select(sp => new CourseDTO
+        //                {
+        //                    Cost = sp.Cost,
+        //                    CourseNo = sp.CourseNo,
+        //                    CreatedBy = sp.CreatedBy,
+        //                    CreatedDate = sp.CreatedDate,
+        //                    Description = sp.Description,
+        //                    ModifiedBy = sp.ModifiedBy,
+        //                    ModifiedDate = sp.ModifiedDate,
+        //                    Prerequisite = sp.Prerequisite,
+        //                    PrerequisiteSchoolId = sp.PrerequisiteSchoolId,
+        //                    SchoolId = sp.SchoolId
+        //                });
 
-        [HttpPost]
-        [Route("GetCourses")]
-        public async Task<DataEnvelope<CourseDTO>> GetCoursesPost([FromBody] DataSourceRequest gridRequest)
-        {
-            DataEnvelope<CourseDTO> dataToReturn = null;
-            IQueryable<CourseDTO> queriableStates = _context.Courses
-                    .Select(sp => new CourseDTO
-                    {
-                        Cost = sp.Cost,
-                        CourseNo = sp.CourseNo,
-                        CreatedBy = sp.CreatedBy,
-                        CreatedDate = sp.CreatedDate,
-                        Description = sp.Description,
-                        ModifiedBy = sp.ModifiedBy,
-                        ModifiedDate = sp.ModifiedDate,
-                        Prerequisite = sp.Prerequisite,
-                        PrerequisiteSchoolId = sp.PrerequisiteSchoolId,
-                        SchoolId = sp.SchoolId
-                    });
+        //        // use the Telerik DataSource Extensions to perform the query on the data
+        //        // the Telerik extension methods can also work on "regular" collections like List<T> and IQueriable<T>
+        //        try
+        //        {
 
-            // use the Telerik DataSource Extensions to perform the query on the data
-            // the Telerik extension methods can also work on "regular" collections like List<T> and IQueriable<T>
-            try
-            {
+        //            DataSourceResult processedData = await queriableStates.ToDataSourceResultAsync(gridRequest);
 
-                DataSourceResult processedData = await queriableStates.ToDataSourceResultAsync(gridRequest);
+        //            if (gridRequest.Groups.Count > 0)
+        //            {
+        //                // If there is grouping, use the field for grouped data
+        //                // The app must be able to serialize and deserialize it
+        //                // Example helper methods for this are available in this project
+        //                // See the GroupDataHelper.DeserializeGroups and JsonExtensions.Deserialize methods
+        //                dataToReturn = new DataEnvelope<CourseDTO>
+        //                {
+        //                    GroupedData = processedData.Data.Cast<AggregateFunctionsGroup>().ToList(),
+        //                    TotalItemCount = processedData.Total
+        //                };
+        //            }
+        //            else
+        //            {
+        //                // When there is no grouping, the simplistic approach of 
+        //                // just serializing and deserializing the flat data is enough
+        //                dataToReturn = new DataEnvelope<CourseDTO>
+        //                {
+        //                    CurrentPageData = processedData.Data.Cast<CourseDTO>().ToList(),
+        //                    TotalItemCount = processedData.Total
+        //                };
+        //            }
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            //fixme add decent exception handling
+        //        }
+        //        return dataToReturn;
+        //    }
 
-                if (gridRequest.Groups.Count > 0)
-                {
-                    // If there is grouping, use the field for grouped data
-                    // The app must be able to serialize and deserialize it
-                    // Example helper methods for this are available in this project
-                    // See the GroupDataHelper.DeserializeGroups and JsonExtensions.Deserialize methods
-                    dataToReturn = new DataEnvelope<CourseDTO>
-                    {
-                        GroupedData = processedData.Data.Cast<AggregateFunctionsGroup>().ToList(),
-                        TotalItemCount = processedData.Total
-                    };
-                }
-                else
-                {
-                    // When there is no grouping, the simplistic approach of 
-                    // just serializing and deserializing the flat data is enough
-                    dataToReturn = new DataEnvelope<CourseDTO>
-                    {
-                        CurrentPageData = processedData.Data.Cast<CourseDTO>().ToList(),
-                        TotalItemCount = processedData.Total
-                    };
-                }
-            }
-            catch (Exception e)
-            {
-                //fixme add decent exception handling
-            }
-            return dataToReturn;
-        }
-
-    }
+        //}
+    } 
 }
